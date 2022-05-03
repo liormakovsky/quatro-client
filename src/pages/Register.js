568a36b5837feb4ddfeab3e446a46da7934cba45
Lior Makovsky <liormakovsky@gmail.com>
2022-05-03 22:48:20 +0300

    continue registration UI

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,20 +28,34 @@ const Register = () => {
     );
   }
 
-  const initialValues = { usr_name: "", email: "", passwordRegister: "" };
+  const initialValues = {
+    usr_name: "",
+    email: "",
+    passwordRegister: "",
+    mailAdvertiseCheckbox: false,
+    firstName: "",
+    lastName: "",
+  };
   const validationSchema = yup.object().shape({
-    email: yup.string().email("המייל לא חוקי").required("המייל הוא שדה חובה"),
+    email: yup.string().email("המייל לא חוקי").required("המייל הינו שדה חובה"),
     passwordRegister: yup
       .string()
-      .required("הסיסמה היא שדה חובה")
+      .required("הסיסמה הינה שדה חובה")
       .min(6, "הסיסמה קצרה מדי, הסיסמה חייבת להכיל לפחות 6 תווים"),
     passwordConfirmation: yup
       .string()
-      .required("אימות ה הסיסמה הוא שדה חובה")
+      .required("אימות הסיסמה הינו שדה חובה")
       .oneOf(
         [yup.ref("passwordRegister"), null],
         "הסיסמה אינה תואמת לסיסמה המקורית"
       ),
+    firstName: yup.string().required("השם הפרטי הינו שדה חובה"),
+    lastName: yup.string().required("שם המשפחה הינו שדה חובה"),
+    homeNumber: yup
+      .number()
+      .typeError("הכנס מספרים בלבד")
+      .required("מספר הבית הינו שדה חובה"),
+    address: yup.string().required("כתובת הינה שדה חובה"),
   });
 
   return (
@@ -107,10 +121,9 @@ const Register = () => {
                         type="password"
                         name="passwordConfirmation"
                         id="passwordConfirmation"
-                        className="login-input password-inputs"
+                        className="login-input doubleInputs"
                         dir="rtl"
                       />
-
                       <ErrorMessage
                         name="passwordConfirmation"
                         component="span"
@@ -118,12 +131,13 @@ const Register = () => {
                         dir="rtl"
                       />
                     </div>
+
                     <div id="password-register-group">
                       <Field
                         type="password"
                         name="passwordRegister"
                         id="passwordRegister"
-                        className="login-input password-inputs"
+                        className="login-input doubleInputs"
                         dir="rtl"
                       />
                       <ErrorMessage
@@ -135,6 +149,91 @@ const Register = () => {
                     </div>
                   </div>
 
+                  <div id="mailAdvertiseSection">
+                    <p>אני מאשר/ת קבלת פרסומים במייל</p>
+                    <div id="mailAdvertiseCheckboxContainer">
+                      <Field
+                        type="checkbox"
+                        name="mailAdvertiseCheckbox"
+                        id="mailAdvertiseCheckbox"
+                      />
+                    </div>
+                  </div>
+                  <div id="deliveryDetailsContainer">
+                    <img
+                      id="deliveryDetailsImg"
+                      src="/png/address-title-text.png"
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="doubleSection">
+                    <div className="doubleInputStart">
+                      <Field
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        className="login-input doubleInputs"
+                        dir="rtl"
+                      />
+                      <ErrorMessage
+                        name="lastName"
+                        component="span"
+                        className="text-danger"
+                        dir="rtl"
+                      />
+                    </div>
+
+                    <div className="doubleInputEnd">
+                      <Field
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        className="login-input doubleInputs"
+                        dir="rtl"
+                      />
+                      <ErrorMessage
+                        name="firstName"
+                        component="span"
+                        className="text-danger"
+                        dir="rtl"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="doubleSection">
+                    <div className="doubleInputStart">
+                      <Field
+                        type="text"
+                        name="homeNumber"
+                        id="homeNumber"
+                        className="login-input doubleInputs"
+                        dir="rtl"
+                      />
+                      <ErrorMessage
+                        name="homeNumber"
+                        component="span"
+                        className="text-danger"
+                        dir="rtl"
+                      />
+                    </div>
+
+                    <div className="doubleInputEnd">
+                      <Field
+                        type="text"
+                        name="address"
+                        id="address"
+                        className="login-input doubleInputs"
+                        dir="rtl"
+                      />
+                      <ErrorMessage
+                        name="address"
+                        component="span"
+                        className="text-danger"
+                        dir="rtl"
+                      />
+                    </div>
+                  </div>
                   <button
                     type="submit"
                     className="btn btn-outline-primary mt-2"
